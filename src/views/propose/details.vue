145d453cf212d8b4ebf52a5df8ea07b38e5c0927
Sianglife <siang7200.huang@gmail.com>
2023-04-20 23:37:07 +0800

    update countdown

--- src/views/propose/details.vue
+++ src/views/propose/details.vue
@@ -15,7 +15,6 @@
                     <span>份)</span>
                 </div>
                 <div v-else>
-                    
                     <div>
                         <span>已附議</span>
                         <span style="color: var(--bs-blue);font-size: 24px;">{{ cardData.num }}</span>
@@ -24,11 +23,15 @@
                     <div>
                         <span style="color: var(--bs-blue);font-size: 24px;">已達到目標50份</span>
                     </div>
-                    <div>
-                        <span>剩餘</span>
-                        <span style="color: var(--bs-blue);font-size: 24px;">{{ 30 - (new Date().getDate() - new Date(cardData.date).getDate()) }}</span>
-                        <span>天</span>
-                    </div>
+                </div>
+                <div v-if="new Date().getDate() - new Date(cardData.date).getDate() <= 30">
+                    <span style="color: var(--bs-blue);font-size: 24px;">附議已截止</span>
+                </div>
+                <div v-else>
+                    <span>剩餘</span>
+                    <span style="color: var(--bs-blue);font-size: 24px;">
+                        {{ 30 - (new Date().getDate() - new Date(cardData.date).getDate()) }}</span>
+                    <span>天</span>
                 </div>
             </div>
             <div style="max-width: calc(100vw); word-break: break-all;">
@@ -37,18 +40,24 @@
                 <h4>簡介</h4>
                 <span style="white-space: pre-wrap;">{{ cardData.details }}</span>
                 <div class="text-center">
-                    
-                    <button v-if="isSupported == false" class="btn btn-primary center" type="button" @click="support">我要附議</button>
-                    <button v-else-if="isSupported == true" class="btn btn-secondary center" type="button" disabled>已附議</button>
-                    <button v-else-if="isSupported == 'adm'" class="btn btn-secondary center" type="button" disabled>管理員不可參與</button>
-                    <button v-else class="btn btn-primary center" type="button" disabled>
+                    <button v-if="new Date().getDate() - new Date(cardData.date).getDate() <= 30"
+                        class="btn btn-primary center" disabled>附議已截止</button>
+                    <button v-else-if="isSupported == 'adm'" class="btn btn-secondary center" type="button"
+                        disabled>管理員不可參與</button>
+                    <button v-else-if="isSupported == undefined" class="btn btn-primary center" type="button" disabled>
                         <div class="spinner-border text-white" role="status"></div>
                     </button>
+                    <div v-else>
+                        <button v-if="isSupported == false" class="btn btn-primary center" type="button"
+                            @click="support">我要附議</button>
+                        <button v-else-if="isSupported == true" class="btn btn-secondary center" type="button"
+                            disabled>已附議</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <commentSec urlPrefix="propose" />
+        <commentSec urlPrefix="propose" />
 </template>
 
 <script>
@@ -62,9 +71,9 @@ export default {
     name: 'proposeDetails',
     data() {
         return {
-            cardData: undefined ,
+            cardData: undefined,
             isSupported: undefined,
-            status:{
+            status: {
                 updating: false,
             }
         }
@@ -73,17 +82,17 @@ export default {
         support() {
             ajax.post(`propose/support/${this.$route.params.id}`, {},
                 (res) => {
-                    if(res.data.status == true){
+                    if (res.data.status == true) {
                         this.isSupported = true;
                         this.getDetails();
-                    }else{
+                    } else {
                         this.isSupported = true;
                     }
                 },
                 (err) => {
                     if (err.response.status == 403) {
                         this.emitter.emit('callLoginShow')
-                    } else if(err.response.status == 401) {
+                    } else if (err.response.status == 401) {
                         alert('管理員不可參與！');
                     } else {
                         console.error(err)
@@ -92,28 +101,28 @@ export default {
         },
         getDetails() {
             ajax.get(`propose/details/${this.$route.params.id}`, {},
-            ((res) => {
-                if(res.data != null){
-                    let data = res.data
-                    ajax.post(`data/getUser`, { id: data.proposer },
-                        ((resUser) => {
-                            data.proposer = resUser.data.name
-                            this.cardData = data
-                            this.$route.meta.title = data.title
-                            document.title = this.$route.meta.title
-                            this.status.updating = true;
-                            this.status.updating = false;
-                        }),
-                        ((err) => {
-                            console.error(err)
-                        }))
-                }else{
-                    this.$router.push({ name: 'Error404', params: { status: 404 }});
-                }
-            }),
-            ((err) => {
-                console.error(err)
-            }))
+                ((res) => {
+                    if (res.data != null) {
+                        let data = res.data
+                        ajax.post(`data/getUser`, { id: data.proposer },
+                            ((resUser) => {
+                                data.proposer = resUser.data.name
+                                this.cardData = data
+                                this.$route.meta.title = data.title
+                                document.title = this.$route.meta.title
+                                this.status.updating = true;
+                                this.status.updating = false;
+                            }),
+                            ((err) => {
+                                console.error(err)
+                            }))
+                    } else {
+                        this.$router.push({ name: 'Error404', params: { status: 404 } });
+                    }
+                }),
+                ((err) => {
+                    console.error(err)
+                }))
         }
     },
     created() {
@@ -122,14 +131,14 @@ export default {
             ((res) => {
                 if (res.data.status == true) {
                     this.isSupported = true
-                } else if(res.data.status == false) {
+                } else if (res.data.status == false) {
                     this.isSupported = false
                 }
             }),
             ((err) => {
                 if (err.response.status == 403) {
                     this.isSupported = false
-                } else if(err.response.status == 401){
+                } else if (err.response.status == 401) {
                     this.isSupported = 'adm'
                 } else {
                     console.error(err)
